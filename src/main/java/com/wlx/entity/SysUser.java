d45c3326547150c84bba0e6bd0a2bffa24b63064
王潞翔 <787746629@qq.com>
2021-08-04 17:31:22 +0800

    更新SysUser类

---
+++ src/main/java/com/wlx/entity/SysUser.java
@@ -0,0 +1,13 @@
+package com.wlx.entity;
+
+/**
+ * 功能模块
+ *
+ * @Author scott
+ * @Date 2021/8/4 13:42
+ */
+public class SysUser {
+    private String username;
+    private String password;
+    private String sex;
+}
